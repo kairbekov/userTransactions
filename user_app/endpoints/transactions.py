4f9e162adc2b395aa4e82ba05cc89010fb7399cf
akayrbekov <abk@frontier.kz>
2022-12-12 13:30:39 +0600

    transaction status changed added

--- user_app/endpoints/transactions.py
+++ user_app/endpoints/transactions.py
@@ -14,31 +14,38 @@ async def get_transactions(db: databases.Database = Depends(get_session)):
 
 @router.get('/transactions/{id}', tags=["transactions"])
 async def get_transaction(id: str, db: databases.Database = Depends(get_session)):
-    user = await models.transactions.get_transaction(db, id)
-    return user
+    res = await models.transactions.get_transaction(db, id)
+    return res
 
 
 @router.post('/transactions/', tags=["transactions"])
 async def create(data: schemas.transactions.Transactions,
                  db: databases.Database = Depends(get_session)):
-    user = await models.transactions.create_transaction(db, data)
-    return user
+    res = await models.transactions.create_transaction(db, data)
+    return res
 
 
 @router.put('/transactions/{id}', tags=["transactions"])
 async def update(id: str, data: schemas.transactions.TransactionsUpdate,
                  db: databases.Database = Depends(get_session)):
-    user = await models.transactions.update_transaction(db, id, data)
-    return user
+    res = await models.transactions.update_transaction(db, id, data)
+    return res
 
 
 @router.delete('/transactions/{id}', tags=["transactions"])
 async def delete(id: str, db: databases.Database = Depends(get_session)):
-    user = await models.transactions.delete_transaction(db, id)
-    return user
+    res = await models.transactions.delete_transaction(db, id)
+    return res
 
 
 @router.get('/transactions/by_user/{id}', tags=["transactions"])
 async def get_all_user_transactions(id: str, db: databases.Database = Depends(get_session)):
-    user = await models.transactions.get_all_user_transactions(db, id)
-    return user
+    res = await models.transactions.get_all_user_transactions(db, id)
+    return res
+
+
+@router.put('/transactions/change_status/{id}', tags=["transactions"])
+async def change_transaction_status(id: str, status: str,
+                                    db: databases.Database = Depends(get_session)):
+    res = await models.transactions.change_transaction_status(db, id, status)
+    return res
--- user_app/models/transactions.py
+++ user_app/models/transactions.py
@@ -5,6 +5,8 @@ import loguru
 import sqlalchemy
 from ..core.db import metadata
 from ..schemas import transactions
+from ..models import users
+from ..schemas.users import UserUpdate
 
 Transactions = sqlalchemy.Table(
     'transactions',
@@ -82,4 +84,26 @@ async def delete_transaction(db: databases.Database, id: str):
 async def get_all_user_transactions(db: databases.Database, user_id: str):
     query = 'Select t.* from transactions t where t.user_id = :user_id'
     res = await db.fetch_one(query, {'user_id': user_id})
+    return res
+
+
+async def change_transaction_status(db: databases.Database, id: str, status: str):
+    data = dict(await get_transaction(db, id))
+    user = dict(await users.get_user(db, data.get('user_id')))
+    user_balance = 0
+
+    if status == transactions.StatusType.confirmed and data.get('status') != status:
+        user_balance = data.get('amount')
+    elif status == transactions.StatusType.declined and data.get('status') != status:
+        user_balance = data.get('amount') * -1
+    elif status == transactions.StatusType.refund and \
+            data.get('status') == transactions.StatusType.confirmed:
+        user_balance = data.get('amount')
+        await create_transaction(db, transactions.Transactions(
+            status=status, user_id=data.get('user_id'), balance=user_balance))
+    res = await update_transaction(db, id, transactions.TransactionsUpdate(status=status))
+    user['balance'] = user.get('balance', 0) + user_balance
+    user.pop('id')
+    await users.update_user(db, data.get('user_id'), UserUpdate(**user))
+
     return res
